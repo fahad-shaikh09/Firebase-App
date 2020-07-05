48ad8d9bc02d600aa86ea7195ad6297c133f5f38
Fahad Shaikh <linkfahad@hotmail.com>
2020-07-06 02:10:10 +0500

    I/come/Expense transactions in Table are being shown

--- transactions.js
+++ transactions.js
@@ -14,7 +14,7 @@ firebase.firestore().collection('users').doc(userID).get()
   console.log("Error", error.message)
 })
 }
-
+// ADD INCOME ON FIREBASE
 document.getElementById("form").addEventListener("submit",addIncome)
 
 function addIncome(e){
@@ -22,7 +22,7 @@ function addIncome(e){
     console.log("addIncome is clicked")
     const userID = localStorage.getItem('userID');
     const amount = document.getElementById("amount").value;
-    const date = document.getElementById("date").value;
+    const date = document.getElementById("date").valueAsDate;
     const description = document.getElementById("description").value;
     const category = document.getElementById("category").value;
 
@@ -36,14 +36,14 @@ function addIncome(e){
         amount, date, description, category, userID,
     })
     .then(function(){
-        alert("Transaction is added in Firebase")
+        alert("Transaction is added in Firebase");
         clearForm();
+        getTransactions();
     })
     .catch(function(error){
-        alert("Transaction is failed with Error: ", error.message)
+        alert("Transaction is failed with Error: ", error.message);
     })  
 }
-
 
 function clearForm(){
     document.getElementById("amount") = "";
@@ -51,3 +51,36 @@ function clearForm(){
     document.getElementById("description") = ""
     document.getElementById("category") = ""
 }
+
+ getTransactions();
+
+function getTransactions() {
+    const table = document.getElementById("transactionTable")
+
+    firebase.firestore().collection("transactions").get()
+    .then(function(snaps){
+        snaps.forEach(function(doc){
+            // console.log("doc.data(): ", doc.data())
+            const data = doc.data()
+            const row = document.createElement('tr')
+            const amount = document.createElement('td')
+            const description = document.createElement('td')
+            const date = document.createElement('td')
+            const category = document.createElement('td')
+
+            amount.innerHTML = data.amount;
+            description.innerHTML = data.description;
+            date.innerHTML = data.date.toDate();
+            category.innerHTML = data.category;
+
+            row.appendChild(amount)
+            row.appendChild(description)
+            row.appendChild(date)
+            row.appendChild(category);
+
+            table.appendChild(row)
+
+        })
+    })
+}
+
